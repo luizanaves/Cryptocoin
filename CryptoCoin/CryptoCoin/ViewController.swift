17d9f83d3d5437dbec66fa041db2b16d3f48c19a
Luiza Negreiros Naves - LNA <luiza.naves@inote554iterislocal.local>
2021-05-05 10:34:49 -0400

    LaunchScreen

---
+++ CryptoCoin/CryptoCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCoin
+//
+//  Created by Luiza Negreiros Naves - LNA on 05/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
