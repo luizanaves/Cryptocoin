d8b46b0ecf06bd70c38aec9d555be2fdcc2150a3
Luiza Negreiros Naves - LNA <luiza.naves@inote554iterislocal.local>
2021-05-05 10:42:39 -0400

    API request

--- CryptoCoin/CryptoCoin/ViewController.swift
+++ CryptoCoin/CryptoCoin/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CoinViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableOfCoins: UITableView!
+    
+    
+    var listOfCoins: [DataCoin] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfCoins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let data = listOfCoins[indexPath.row]
+        
+        cell.textLabel?.text = "\(data.name) - \(data.symbol)"
+        cell.detailTextLabel?.text = "\(data.quote.USD.price)"
+        
+    return cell
+    }
 }
 
--- CryptoCoin/CryptoCoin/data/request/CoinRequest.swift
+++ CryptoCoin/CryptoCoin/data/request/CoinRequest.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+enum CoinError: Error {
+    case noDataAvaliable
+    case canNotProcessData
+    
+}
+
+struct CoinRequest {
+    let resourceURL: URL
+    let API_HEADER = "X-CMC_PRO_API_KEY"
+    let API_KEY = "7d40fdff-190e-424b-90bf-37c7ee6e696a"
+    
+    init(CoinCode: String) {
+        let resourceString = "https://pro-api.coinmarketcap.com/cryptocurrency/listings/latest?\(API_HEADER)=\(API_KEY)"
+        guard let responseURL = URL(string: resourceString) else {fatalError()}
+        
+        self.resourceURL = responseURL
+    }
+    
+    func getCoins (completion: @escaping(Result<[DataCoin], CoinError>) -> Void){
+        let coinTask = URLSession.shared.dataTask(with: resourceURL){
+            datas,_,_ in
+            guard let jsonData = datas else {
+                completion(.failure(.noDataAvaliable))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let coinResponse = try decoder.decode(Coin.self, from: jsonData)
+                let coinDetails = coinResponse.data
+                completion(.success(coinDetails))
+            }
+            catch {
+                completion(.failure(.canNotProcessData))
+            }
+        }
+        coinTask.resume()
+    }
+}
